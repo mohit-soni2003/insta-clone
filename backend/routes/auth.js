e07f17b5f212d68fbd657465bc2ce6fb24ff194b
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-06-29 15:23:23 +0530

    signin api is developed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,4 +47,30 @@ router.post("/signup",(req,res)=>{
    
 })
 
+
+router.post("/signin",(req,res)=>{
+    const {email , password} = req.body;
+
+    if(!email , !password){
+        return res.status(422).json({error : "Please Add Email and Password"})
+    }
+    
+    USER.findOne({email:email})
+    .then((savedUser)=>{
+        if(!savedUser){
+            return res.status(422).json({error : "Invalid Email"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then((match)=>{
+            if(match){
+                return res.status(422).json({message : "Signin Successful"})
+            }
+            else{
+                return res.status(422).json({error : "Invalid Password"})
+            }
+        })
+        .catch(err=>console.log(err))
+    })
+
+})
 module.exports = router
--- frontend/insta-clone/src/components/Signin.jsx
+++ frontend/insta-clone/src/components/Signin.jsx
@@ -1,17 +1,74 @@
 import React from 'react'
 import "./Signin.css"
 import Logo from "../img/Logo.png";
-import { Link } from "react-router-dom";
+import { Link , Navigate, useNavigate} from "react-router-dom";
+import { useEffect, useState  } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+
+
+
+
 
 
 
 export default function Signin() {
+
+  const navigate = useNavigate()
+
+
+//State variable of signin -------- ------ ------ --------- ------
+
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+
+  let handleEmailChange =(event)=>{
+    console.log(event.target);
+    setemail(event.target.value)
+  };
+  let handlePasswordChange =(event)=>{
+    console.log(event.target);
+    setpassword(event.target.value)
+  };
+
+// Post data function ------ -------- ------- --------- -------- -
+
+const postData = ()=>{
+
+  fetch("http://localhost:8080/signin",
+      {
+          method : "post",
+          headers:{
+              "Content-Type" : "application/json"
+          },
+          body: JSON.stringify({
+            email:email,
+            password:password
+          })
+      }
+  ).then(res=>res.json())
+  .then(data =>{
+      if(data.message){
+          notifyA(data.message)
+          navigate("/")
+        }
+        
+      if(data.error){
+          notifyB(data.error)
+      }
+      console.log(data)})
+  
+//Toast Function --------- ------ --------- -------
+
+const notifyA = (msg)=>toast.success(msg)
+const notifyB = (msg)=>toast.error(msg)
+}
+
   return (<>
     <div className='signin' >
       <img src={Logo} alt="" />
-      <input type="text" name='username' placeholder='Phone number , username or email'/>
-      <input type="text" name='password' placeholder='Password' />
-      <button className='login-btn'>Log in</button>
+      <input type="text" name='email' onChange={handleEmailChange}  placeholder='Phone number , username or email'/>
+      <input type="text" name='password' onChange={handlePasswordChange} placeholder='Password' />
+      <button className='login-btn' onClick={postData}>Log in</button>
       <div className="or">-------OR---------</div>
       <a href="">Log in with Facebook</a>
       <div className="forgot-pass"><Link to="">Forgot Password</Link></div>
