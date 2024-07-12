5e2a7e9ed383e3e4041384a37890b1ba5f9fd5cc
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-07-12 23:40:18 +0530

    Post fetched on Home page

--- frontend/insta-clone/src/components/Home.jsx
+++ frontend/insta-clone/src/components/Home.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 import "./Home.css"
 export default function Home() {
     const navigate = useNavigate();
+    const [data, setdata] = useState([])
+
     useEffect(() => {
         const token = localStorage.getItem("jwt")
         if (!token) {
@@ -14,9 +16,9 @@ export default function Home() {
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("jwt")
             },
-        }).then(res=>res.json())
-        .then(result=>console.log(result))
-        .catch(err=>console.log(err))
+        }).then(res => res.json())
+            .then(result => setdata(result))
+            .catch(err => console.log(err))
     }, [])
 
 
@@ -25,32 +27,38 @@ export default function Home() {
         <div className="home">
             {/* card */}
 
-            <div className="card">
-                {/* card-header */}
-                <div className="card-header">
-                    <div className="card-pic">
-                        <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmUlMjBzcXVhcmV8ZW58MHx8MHx8fDA%3D" alt="" />
-                    </div>
-                    <h5>Ramesh</h5>
-                </div>
-                {/* card image */}
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1556740772-1a741367b93e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMHBpY3R1cmUlMjBzcXVhcmV8ZW58MHx8MHx8fDA%3D" alt="" />
-                </div>
-                {/* card-content */}
-                <div className="card-content">
-                    <span className="material-symbols-outlined">favorite</span>
-                    <p>1 Like</p>
-                    <p>this is caption</p>
-                </div>
-                {/* card-comment */}
-                <div className="add-comment">
-                    <span className="material-symbols-outlined smiley-icon">mood</span>
-                    <input type="text" placeholder="Add a comment" />
-                    <button className="comment">Post</button>
+            {data.map((posts) => {
+                console.log(posts)
+                return (
+                    <div className="card">
+                        {/* card-header */}
+                        <div className="card-header">
+                            <div className="card-pic">
+                                <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZSUyMHBpY3R1cmUlMjBzcXVhcmV8ZW58MHx8MHx8fDA%3D" alt="" />
+                            </div>
+                            <h5>{posts.postedBy.name}</h5>
+                        </div>
+                        {/* card image */}
+                        <div className="card-image">
+                            <img src={posts.photo} alt="" />
+                        </div>
+                        {/* card-content */}
+                        <div className="card-content">
+                            <span className="material-symbols-outlined">favorite</span>
+                            <p>1 Likes</p>
+                            <p>{posts.body}</p>
+                        </div>
+                        {/* card-comment */}
+                        <div className="add-comment">
+                            <span className="material-symbols-outlined smiley-icon">mood</span>
+                            <input type="text" placeholder="Add a comment" />
+                            <button className="comment">Post</button>
 
-                </div>
-            </div>
+                        </div>
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
